9892362187f54ceb39592e73c4e22f82520c3597
RobinLinus <robinlinus@users.noreply.github.com>
2020-02-11 11:56:56 +0100

    Cleanup

--- src/merkle-set/test.js
+++ src/merkle-set/test.js
@@ -1,11 +1,11 @@
 import { merkleRoot } from './merkle-set.js'
-import { SHA256 } from './../../../hash-js/hash.js'
+import { SHA256d } from './../../../hash-js/hash.js'
 
-describe('Merkle Tree', function() {
+describe('The merkle-set lib', function() {
     it('can compute a Merkle root', async function() {
-        const hash1 = SHA256.fromUnicode('abc')
-        const hash2 = SHA256.fromUnicode('def')
-        const root = new SHA256(await merkleRoot([hash1, hash2]))
-        expect(root.toHex()).toBe('8e261adb033e68cc737c46085039ac2d96d4f4fe382d014d84b280585bd80973');
-    });
-});
+        const hash1 = SHA256d.fromUnicode('abc')
+        const hash2 = SHA256d.fromUnicode('def')
+        const root = new SHA256d(await merkleRoot([hash1, hash2]))
+        expect(root.toHex()).toBe('8e261adb033e68cc737c46085039ac2d96d4f4fe382d014d84b280585bd80973')
+    })
+})
